8787747fa4d068cf253d572b3703bb6d52eb4b77
ToxaKaz <anton.a.kazancev@gmail.com>
2022-10-28 00:00:15 +0300

    AST documentation attributes added

--- Pascal/lib/ast.mli
+++ Pascal/lib/ast.mli
@@ -7,105 +7,121 @@ open ImArray
 
 type name = string
 
+(** binary operators *)
 type binop =
-  | Add
-  | Sub
-  | Mul
-  | Div
-  | Mod
-  | FDiv
-  | And
-  | Or
-  | Xor
-  | Greater
-  | Less
-  | Eq
-  | NotEq
-  | GreaterEq
-  | LessEq
-  | RShift
-  | LShift
+  | Add (** ( + ) *)
+  | Sub (** ( - ) *)
+  | Mul (** ( * ) *)
+  | Div (** ( div ) *)
+  | Mod (** ( mod ) *)
+  | FDiv (** ( / ) *)
+  | And (** ( and ) *)
+  | Or (** ( or ) *)
+  | Xor (** ( xor ) *)
+  | Greater (** ( > ) *)
+  | Less (** ( < ) *)
+  | Eq (** ( = ) *)
+  | NotEq (** ( <> ) *)
+  | GreaterEq (** ( >= ) *)
+  | LessEq (** ( <= ) *)
+  | RShift (** ( bit shift right ) *)
+  | LShift (** ( bit shift left ) *)
 [@@deriving show]
 
+(** unar operators *)
 type unop =
-  | Plus
-  | Minus
-  | Not
+  | Plus (** + *)
+  | Minus (** - *)
+  | Not (** not *)
 [@@deriving show]
 
+(** virtual types *)
 type vtype =
-  | VTBool
-  | VTInt
-  | VTFloat
-  | VTChar
-  | VTString of expr
-  | VTNDString
-  | VTDString of int
-  | VTRecord of (name * vtype) list
-  | VTDRecord of vtype KeyMap.t
-  | VTFunction of fun_param list * vtype
-  | VTArray of expr * expr * vtype
+  | VTBool (** boolean type *)
+  | VTInt (** integer type *)
+  | VTFloat (** real type *)
+  | VTChar (** char type *)
+  | VTString of expr (** string type with expr len *)
+  | VTNDString (** string type without len, will be used 255 *)
+  | VTDString of int (** string type with len *)
+  | VTRecord of (name * vtype) list (** record type of definitions *)
+  | VTDRecord of vtype KeyMap.t (** record type constructed *)
+  | VTFunction of fun_param list * vtype (** function type *)
+  | VTArray of expr * expr * vtype (** array type with expr..expr interval *)
   | VTDArray of value * int * vtype
-  | VTCustom of name
-  | VTCollable of name
-  | VTVoid
+      (** array type with calculated interval : (from <val> with <size> of <type>) *)
+  | VTCustom of name (** custom type *)
+  | VTCollable of name (** special type for std function name before call *)
+  | VTVoid (** special type for procedure declaration *)
 [@@deriving show]
 
+(** virtual values *)
 and value =
-  | VBool of bool
-  | VInt of int
-  | VFloat of float
-  | VChar of char
-  | VString of string
-  | VRecord of world
+  | VBool of bool (** boolean value *)
+  | VInt of int (** integer value *)
+  | VFloat of float (** real value *)
+  | VChar of char (** char value *)
+  | VString of string (** string value *)
+  | VRecord of world (** record value *)
   | VFunction of name * vtype * fun_param list * world * statement list
+      (** function value : (<name> <result type> <param list> <local world> <realization>) *)
   | VArray of value * int * vtype * value ImArray.t
-  | VCollable of name
+      (** array value : from <val> with <size> of <type>, contains <arr>*)
+  | VCollable of name (** special value for std function name before call *)
   | VVoid
+      (** special type for procedure declaration or for an unassigned function variable *)
 [@@deriving show]
 
+(** function parameters *)
 and fun_param =
-  | FPFree of name * vtype
-  | FPOut of name * vtype
-  | FPConst of name * vtype
+  | FPFree of name * vtype (** standard parameter *)
+  | FPOut of name * vtype (** out parameter *)
+  | FPConst of name * vtype (** const parameter *)
 [@@deriving show]
 
+(** expression *)
 and expr =
-  | Const of value
-  | Variable of name
-  | BinOp of binop * expr * expr
-  | UnOp of unop * expr
-  | Call of expr * expr list
-  | GetRec of expr * name
-  | GetArr of expr * expr
+  | Const of value (** const *)
+  | Variable of name (** variable *)
+  | BinOp of binop * expr * expr (** binary operator *)
+  | UnOp of unop * expr (** unary operator *)
+  | Call of expr * expr list (** function calling *)
+  | GetRec of expr * name (** record field getting *)
+  | GetArr of expr * expr (** array field getting *)
 [@@deriving show]
 
+(** statement *)
 and statement =
-  | Assign of expr * expr
-  | ProcCall of expr
-  | If of expr * statement list * statement list
-  | While of expr * statement list
-  | Repeat of expr * statement list
-  | For of name * expr * expr * statement list
-  | Break
-  | Continue
+  | Assign of expr * expr (** assignment *)
+  | ProcCall of expr (** procedure calling *)
+  | If of expr * statement list * statement list (** if statement *)
+  | While of expr * statement list (** while loop *)
+  | Repeat of expr * statement list (** repeat-until loop *)
+  | For of name * expr * expr * statement list (** for loop with counter *)
+  | Break (** break *)
+  | Continue (** continue *)
 [@@deriving show]
 
+(** definition *)
 and define =
-  | DType of name * vtype
-  | DNDVariable of name * vtype
-  | DVariable of name * vtype * expr
-  | DDVariable of name * vtype * value
-  | DConst of name * expr
-  | DDConst of name * value
-  | DFunction of name * vtype * fun_param list * t
+  | DType of name * vtype (** type definition *)
+  | DNDVariable of name * vtype (** variable definition without assignment *)
+  | DVariable of name * vtype * expr (** variable definition with assignment by expr *)
+  | DDVariable of name * vtype * value (** variable definition with assignment by value *)
+  | DConst of name * expr (** const definition by expr *)
+  | DDConst of name * value (** const definition by value *)
+  | DFunction of name * vtype * fun_param list * t (** function definition *)
 [@@deriving show]
 
+(** world item *)
 and variable =
-  | VConst of value
-  | VVariable of value
-  | VType
+  | VConst of value (** constant *)
+  | VVariable of value (** variable *)
+  | VType (** type *)
 [@@deriving show]
 
+(** world, which contains all info about variables *)
 and world = (vtype * variable) KeyMap.t [@@deriving show]
+
+(** Pascal program of definition list and statement list *)
 and t = define list * statement list [@@deriving show]
