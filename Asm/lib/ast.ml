38a12ae23859c65c47102b5682449a8ca25e765a
Andrei Izrailev <Andrei.Izrailev@arknetworks.am>
2022-11-19 19:21:35 +0400

    Add functions to get and set values of registers in reg_map
    Signed-off-by: Andrei Izrailev <Andrei.Izrailev@arknetworks.am>

--- Asm/lib/ast.ml
+++ Asm/lib/ast.ml
@@ -38,6 +38,14 @@ module OperandsHandler : sig
   val reg_name_to_byte_reg : string -> byte reg
   val reg_name_to_word_reg : string -> word reg
   val reg_name_to_dword_reg : string -> dword reg
+
+  (* These two functions require us to have ids of all dword registers as keys
+     in the reg_map *)
+  val reg_val_get : 'a reg -> int IntMap.t -> int
+
+  (* If the value to set is too big, only the last bytes are considered.
+     E.g. if we try to set "ah" to 0xABC, it will be set to 0xBC *)
+  val reg_val_set : 'a reg -> int -> int IntMap.t -> int IntMap.t
 end = struct
   type byte
   type word
@@ -125,6 +133,119 @@ end = struct
   let reg_name_to_byte_reg reg_name = reg_name |> reg_name_to_id |> int_to_byte_reg
   let reg_name_to_word_reg reg_name = reg_name |> reg_name_to_id |> int_to_word_reg
   let reg_name_to_dword_reg reg_name = reg_name |> reg_name_to_id |> int_to_dword_reg
+
+  (* Get id of a corresponding primary register, e.g. for id of "ch" this
+     function returns id of "ecx" *)
+  let reg_get_primary_id reg =
+    let reg_id = reg_to_id reg in
+    if reg_id_is_dword_reg reg_id
+    then reg_id
+    else if reg_id_is_word_reg reg_id
+    then reg_id + word_reg_list_len
+    else (reg_id / 2) + byte_reg_list_len + word_reg_list_len
+  ;;
+
+  let reg_val_get reg reg_map =
+    let reg_id = reg_to_id reg in
+    let primary_id = reg_get_primary_id reg_id in
+    let primary_val = IntMap.find primary_id reg_map in
+    if reg_id_is_dword_reg reg_id
+    then primary_val
+    else if reg_id_is_word_reg reg_id
+    then primary_val land 0xFFFF
+    else if reg_id % 2 = 1 (* Our register is like "al" *)
+    then primary_val land 0xFF
+    else (primary_val land 0xFF00) lsr 8
+  ;;
+
+  let reg_val_set reg value reg_map =
+    let reg_id = reg_to_id reg in
+    let primary_id = reg_get_primary_id reg_id in
+    let primary_value = IntMap.find primary_id reg_map in
+    let value_to_set =
+      if reg_id_is_dword_reg reg_id
+      then value land 0xFFFFFFFF
+      else if reg_id_is_word_reg reg_id
+      then ((primary_value lsr 16) lsl 16) lor (value land 0xFFFF)
+      else if reg_id % 2 = 1
+      then ((primary_value lsr 8) lsl 8) lor (value land 0xFF)
+      else
+        ((primary_value lsr 16) lsl 16)
+        lor (((value land 0xFF) lsl 8) lor (primary_value land 0xFF))
+    in
+    IntMap.add primary_id value_to_set reg_map
+  ;;
+
+  module RegMapTest = struct
+    open IntMap
+
+    let%test _ =
+      reg_get_primary_id (reg_name_to_dword_reg "ecx") = reg_name_to_dword_reg "ecx"
+    ;;
+
+    let%test _ =
+      reg_get_primary_id (reg_name_to_word_reg "bx") = reg_name_to_dword_reg "ebx"
+    ;;
+
+    let%test _ =
+      reg_get_primary_id (reg_name_to_byte_reg "dh") = reg_name_to_dword_reg "edx"
+    ;;
+
+    let%test _ =
+      reg_get_primary_id (reg_name_to_byte_reg "al") = reg_name_to_dword_reg "eax"
+    ;;
+
+    let reg_map =
+      empty
+      |> add (reg_name_to_dword_reg "eax") 0xABCD1234
+      |> add (reg_name_to_dword_reg "ebx") 0
+      |> add (reg_name_to_dword_reg "ecx") 0
+      |> add (reg_name_to_dword_reg "edx") 0
+    ;;
+
+    let%test _ = reg_val_get (reg_name_to_dword_reg "eax") reg_map = 0xABCD1234
+    let%test _ = reg_val_get (reg_name_to_word_reg "ax") reg_map = 0x1234
+    let%test _ = reg_val_get (reg_name_to_byte_reg "al") reg_map = 0x34
+    let%test _ = reg_val_get (reg_name_to_byte_reg "ah") reg_map = 0x12
+
+    let reg_map = reg_map |> add (reg_name_to_dword_reg "ecx") 0xABCD1234
+
+    let%test _ =
+      reg_val_set (reg_name_to_byte_reg "ch") 0x10 reg_map
+      |> reg_val_get (reg_name_to_dword_reg "ecx")
+      = 0xABCD1034
+    ;;
+
+    let%test _ =
+      reg_val_set (reg_name_to_byte_reg "cl") 0x0 reg_map
+      |> reg_val_get (reg_name_to_dword_reg "ecx")
+      = 0xABCD1200
+    ;;
+
+    let%test _ =
+      reg_val_set (reg_name_to_word_reg "cx") 0x59B7 reg_map
+      |> reg_val_get (reg_name_to_dword_reg "ecx")
+      = 0xABCD59B7
+    ;;
+
+    let%test _ =
+      reg_val_set (reg_name_to_dword_reg "edx") 0x7654321A reg_map
+      |> reg_val_get (reg_name_to_dword_reg "edx")
+      = 0x7654321A
+    ;;
+
+    let%test _ =
+      reg_val_set (reg_name_to_byte_reg "ah") 0xABC reg_map
+      |> reg_val_get (reg_name_to_dword_reg "eax")
+      = 0xABCDBC34
+    ;;
+
+    let%test _ =
+      reg_val_set (reg_name_to_word_reg "ax") 0xABCDEF reg_map
+      |> reg_val_get (reg_name_to_dword_reg "eax")
+      = 0xABCDCDEF
+    ;;
+  end
 end
 
 open OperandsHandler
