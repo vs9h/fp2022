a44bfa3255e9b7c854fe1248a6b6e61a4cbf9f5f
Ivan Morozko <Ivan.Morozko@oktetlabs.ru>
2022-11-20 22:53:50 +0400

    add timestamps checking to interpreter
    Signed-off-by: Ivan Morozko <Ivan.Morozko@oktetlabs.ru>

--- Make/lib/interpret.ml
+++ Make/lib/interpret.ml
@@ -51,6 +51,17 @@ let remove_duplicates l =
   List.rev (List.fold_left cons_uniq [] l)
 ;;
 
+(* Return true if a target is present somewhere in targets in rules *)
+let rules_has_target target rules =
+  Printf.printf "Checking %s\n" target;
+  let rule_has_target rule =
+    Option.is_some (List.find_opt (String.equal target) rule.targets)
+  in
+  let rules_with_target = List.filter rule_has_target rules in
+  Printf.printf "ans is %b\n" (List.length rules_with_target = 0);
+  List.length rules_with_target <> 0
+;;
+
 (* Return target data among all rules for specified target. *)
 (* If two or more rules are found with the same target in the target list, *)
 (* and with different recipes, the newer one will be used *)
@@ -125,15 +136,24 @@ type dfs_info =
   ; dfs_parents : string NodeMap.t (* parent map *)
   ; dfs_time : int (* most recent time *)
   ; dfs_edgeList : string -> string list
+  ; dfs_recompile : bool NodeMap.t (* Should we recompile this target? *)
+  ; rules : rule_as_lists list
   ; graph : graph
   }
 
-let dfs_init_info graph =
+let dfs_init_info rules graph =
+  let edgeList node =
+    match Hashtbl.find_opt graph node with
+    | None -> []
+    | Some data -> data.deps
+  in
   { dfs_dtime = NodeMap.empty
   ; dfs_ftime = NodeMap.empty
   ; dfs_parents = NodeMap.empty
   ; dfs_time = 0
-  ; dfs_edgeList = (fun node -> (Hashtbl.find graph node).deps)
+  ; dfs_edgeList = edgeList (*(fun node -> .deps)*)
+  ; dfs_recompile = NodeMap.empty
+  ; rules
   ; graph
   }
 ;;
@@ -155,7 +175,7 @@ let dfs_color info node =
 (* increment time *)
 let dfs_inc_time info = { info with dfs_time = info.dfs_time + 1 }
 
-(* increment time, set discover time for node, mark node Gray *)
+(* increment time, set discover time for node, mark node Gray, set bit to false *)
 let dfs_discover info node =
   let info = dfs_inc_time info in
   let dtime = NodeMap.add node info.dfs_time info.dfs_dtime in
@@ -175,6 +195,18 @@ let dfs_add_parent info node parent =
   { info with dfs_parents = parents }
 ;;
 
+(* set flag for node *)
+let dfs_set_flag info node flag =
+  let recompile = NodeMap.add node flag info.dfs_recompile in
+  { info with dfs_recompile = recompile }
+;;
+
+let flag_of_node info node =
+  match NodeMap.find_opt node info.dfs_recompile with
+  | None -> false
+  | Some flag -> flag
+;;
+
 (* Updates info in a way that node y is no longer a neighbour of node x *)
 let drop_dependency info x y =
   let new_dfs_edgeList node =
@@ -182,13 +214,17 @@ let drop_dependency info x y =
     then List.filter (y |> String.equal |> Fun.negate) (info.dfs_edgeList node)
     else info.dfs_edgeList node
   in
-  { dfs_dtime = info.dfs_dtime
-  ; dfs_ftime = info.dfs_ftime
-  ; dfs_parents = info.dfs_parents
-  ; dfs_time = info.dfs_time
-  ; dfs_edgeList = new_dfs_edgeList
-  ; graph = info.graph
-  }
+  { info with dfs_edgeList = new_dfs_edgeList }
+;;
+
+(* set recompile flag for all parents of target (but not for the target itself) *)
+let set_recompile_flag_for_all_parents info target =
+  let rec set_flag info t =
+    match NodeMap.find_opt t info.dfs_parents with
+    | None -> info
+    | Some parent -> set_flag (dfs_set_flag info parent true) parent
+  in
+  set_flag info target
 ;;
 
 let print_cycle parents node =
@@ -202,62 +238,139 @@ let print_cycle parents node =
   print_endline " dependency dropped."
 ;;
 
+(*
+                    Algorithm to decide whether we should recompile target X
+
+                                   ┌────────────────────┐  no
+                                   │Should recompile flg├───────┐
+                                   └───────┬────────────┘       │
+                                        yes│                    │
+                                           │                    │
+                                      ┌────▼─────┐       yes ┌──▼──────┐   no
+                      ┌───────────────┤ recompile│      ┌────┤X exists?├──────┐
+                      │               └──────────┘      │    └─────────┘      │
+               ┌──────▼──────┐   no                     │                     │
+               │ No recipes? ├───────────►end      ┌────▼─────────────┐   ┌───▼────────┐ yes
+               └─────┬───────┘                     │get timestamp     │   │Rule in Ast?├────┐
+                     │                             │of X and parent(X)│   └─────┬──────┘    │
+       ┌─────────┐   │                             └────┬─────────────┘      no │           │
+   yes │X is main◄───┘                                  │               ┌───────▼─┐     ┌───▼─────┐
+     ┌─┤ target? │                     ┌──────────────┐ │               │X is main│     │recompile│
+     │ └──┬──────┘                     │X > parent(X)?├─┘               │ target? │     └─────┬───┘
+     │    │no                          └─┬──────────┬─┘                 └┬───────┬┘           │
+     │    └──────────►end             yes│          │ no              yes│       │no          │
+     │                                   │          │                    │       ▼            │
+     │                                   │          └─────────► end      │    raise NoRule    │
+     │                     ┌─────────────▼────────┐                      │                    │
+┌────▼────────────────┐    │Set recompile flag for│                      │            ┌───────▼─────┐
+│raise NothingToBeDone│    │   all parents of X   │                      │            │ No recipes? │
+└─────────────────────┘    └────────────┬─────────┘                      │            └───────┬─────┘
+                                        │                                │                    │
+                                        │                                │              ┌─────▼───┐
+                                        ▼                                │          yes │X is main│  no
+                                       end                               ├──────────────┤ target? ├────────►end
+                                                                         │              └─────────┘
+                                                                ┌────────▼────────────┐
+                                                                │raise NothingToBeDone│
+                                                                └─────────────────────┘
+
+
+One small fix, cause main target doesn't have a parent:
+
+                      ┌─────────┐          yes ┌─────────┐
+ ┌──────────────┐ yes │X is main◄──────────────┤X exists?│
+ │raise UpToDate◄─────┤ target? │              └─────────┘
+ └──────────────┘     └──┬──────┘
+                         │no        ┌──────────────────┐
+                         └─────────►┘get timestamp     │
+
+ *)
+
 exception Recipe of string * int
+exception NoRule of string * string
+exception UpToDate of string
+exception NothingToBeDone of string
+
+let get_recipes info target =
+  match Hashtbl.find_opt info.graph target with
+  | None -> []
+  | Some data -> data.recipes
+;;
+
+let recompile info target is_main_target =
+  let recipes = get_recipes info target in
+  let execute_recipes =
+    let rec traverse_recipes = function
+      | [] -> info
+      | recipe :: tl ->
+        print_endline recipe;
+        let rc = Sys.command recipe in
+        if rc <> 0 then raise (Recipe (target, rc)) else traverse_recipes tl
+    in
+    traverse_recipes recipes
+  in
+  if List.length recipes = 0
+  then if is_main_target then raise (NothingToBeDone target) else info
+  else execute_recipes
+;;
 
 (* Could raise exception *)
-let execute_recipes graph target =
-  let rec traverse_recipes = function
-    | [] -> ()
-    | recipe :: tl ->
-      let rc = Sys.command recipe in
-      if rc <> 0 then raise (Recipe (target, rc)) else traverse_recipes tl
-  in
-  traverse_recipes (Hashtbl.find graph target).recipes
+let try_execute_recipes info target is_main_target =
+  if flag_of_node info target
+  then recompile info target is_main_target
+  else if Sys.file_exists target
+  then
+    if is_main_target
+    then raise (UpToDate target)
+    else (
+      let parent = NodeMap.find target info.dfs_parents in
+      if Sys.file_exists parent = false
+      then set_recompile_flag_for_all_parents info target
+      else (
+        let parent_time = (Unix.stat parent).st_mtime in
+        let target_time = (Unix.stat target).st_mtime in
+        if target_time > parent_time
+        then set_recompile_flag_for_all_parents info target
+        else info))
+  else if rules_has_target target info.rules
+  then recompile info target is_main_target
+  else if is_main_target
+  then raise (NothingToBeDone target)
+  else raise (NoRule (target, NodeMap.find target info.dfs_parents))
 ;;
 
 (* Traverse graph from node u, executing recipes of u in the end *)
 (* Could raise exception *)
 let rec dfs_visit info u =
-  Printf.printf "running dfs from node %s\n" u;
   (* Visit all neighbours v of node u *)
   let visit_node info v =
-    Printf.printf "Looking at %s -- neighbour of %s\n" v u;
-    if dfs_color info v = Black
+    let info = dfs_add_parent info v u in
+    if dfs_color info v = Gray (* on recursion stack -> cycle found *)
     then (
-      Printf.printf "%s is black, skipping\n" v;
-      info)
+      print_cycle info.dfs_parents v;
+      drop_dependency info u v)
     else (
-      let info = dfs_add_parent info v u in
-      if dfs_color info v = Gray (* on recursion stack -> cycle found *)
-      then (
-        Printf.printf "%s is gray, cycle found.\n" v;
-        print_cycle info.dfs_parents v;
-        drop_dependency info u v)
-      else (
-        Printf.printf "%s is white, going in it\n" v;
-        let info = dfs_discover info v in
-        (* recurse on v *)
-        let info = dfs_visit info v in
-        let info = dfs_finish info v in
-        Printf.printf "%s traversal finished\n" v;
-        execute_recipes info.graph v;
-        info))
+      let info = dfs_discover info v in
+      (* recurse on v *)
+      let info = dfs_visit info v in
+      let info = dfs_finish info v in
+      try_execute_recipes info v false)
   in
   List.fold_left visit_node info (info.dfs_edgeList u)
 ;;
 
 (* Could raise exception *)
-let dfs graph target =
-  let info = dfs_init_info graph in
+let dfs rules graph target =
+  let info = dfs_init_info rules graph in
   let info = dfs_discover info target in
-  let _ = dfs_visit info target in
-  execute_recipes graph target
+  let info = dfs_visit info target in
+  try_execute_recipes info target true
 ;;
 
 (* Could raise exception *)
 let build_target rules target =
   let graph = graph_of_rules rules in
-  dfs graph target
+  dfs rules graph target
 ;;
 
 let interpret (ast : ast) targets =
@@ -267,31 +380,23 @@ let interpret (ast : ast) targets =
     | [] -> Ok ""
     | target :: tl ->
       (try
-         build_target rules target;
+         let _ = build_target rules target in
          traverse_targets tl
        with
        | Recipe (target, rc) ->
-         Error (Printf.sprintf "make: *** [Makefile: %s] Error %d" target rc))
+         Error (Printf.sprintf "make: *** [Makefile: '%s'] Error %d" target rc)
+       | NoRule (target, main_target) ->
+         Error
+           (Printf.sprintf
+              "make: *** No rule to make target '%s', needed by %s.  Stop."
+              target
+              main_target)
+       | UpToDate target -> Ok (Printf.sprintf "make: '%s' is up to date." target)
+       | NothingToBeDone target ->
+         Ok (Printf.sprintf "make: Nothing to be done for '%s'." target))
   in
   match targets with
   (* When no targets are specified, choose first one *)
   | [] -> traverse_targets [ rules |> get_unique_filenames |> List.hd ]
   | _ -> traverse_targets targets
-;;
-
-let rules =
-  [ { targets = [ "b"; "c" ]; prerequisites = [ "d"; "e" ]; recipes = [ "echo a" ] }
-  ; { targets = [ "a" ]; prerequisites = [ "e" ]; recipes = [ "echo e" ] }
-  ; { targets = [ "d"; "x" ]; prerequisites = [ "b"; "i"; "k" ]; recipes = [ "echo d" ] }
-  ; { targets = [ "e" ]; prerequisites = [ "k" ]; recipes = [ "echo e" ] }
-  ; { targets = [ "e" ]; prerequisites = [ "i" ]; recipes = [ "echo a" ] }
-  ]
-;;
-
-let%test _ =
-  let _ =
-    try build_target rules "b" with
-    | Recipe (target, rc) -> Printf.printf "make: *** [Makefile: %s] Error %d" target rc
-  in
-  false
 ;;
