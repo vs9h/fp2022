be806173c2e5f9a3517cf04df98a2d570a1113a7
Ivan Morozko <Ivan.Morozko@oktetlabs.ru>
2022-11-21 01:31:10 +0400

    add test that execs makefile in test_project folder
    Signed-off-by: Ivan Morozko <Ivan.Morozko@oktetlabs.ru>

---
+++ Make/demo/demoInterpret.ml
@@ -0,0 +1,21 @@
+(** Copyright 2021-2022, Morozko Ivan *)
+
+(** SPDX-License-Identifier: CC0-1.0 *)
+
+open Make_lib
+open Core
+
+let _ =
+  Stdlib.Sys.chdir "test_project";
+  let () =
+    let input = In_channel.read_all "./Makefile" in
+    match Parser.parse input with
+    | Result.Ok ast ->
+      Format.printf "%a\n%!\n" Ast.pp_ast ast;
+      (match Interpret.interpret ast [ ] with
+       | Result.Ok str -> print_string str
+       | Error str -> print_string str)
+    | Error m -> Format.printf "Parse error \n%s!" m
+  in
+  ()
+;;
