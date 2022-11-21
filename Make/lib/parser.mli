ac1466744bb510647b12e4fa95c8ba06b2b8e1b0
Ivan Morozko <Ivan.Morozko@oktetlabs.ru>
2022-11-21 16:04:17 +0400

    add license plate
    Signed-off-by: Ivan Morozko <Ivan.Morozko@oktetlabs.ru>

--- Make/lib/parser.mli
+++ Make/lib/parser.mli
@@ -1,4 +1,6 @@
 (** Copyright 2021-2022, ol-imorozko and contributors *)
 
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
 (** Main entry of parser *)
 val parse : string -> (Ast.ast, string) result
